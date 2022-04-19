063ab2061e1b1d70a1ca13a07d3e3403e527637c
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-04-19 16:57:08 -0300

    Iniciando app/ pagina splash

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_key_in_widget_constructors
+import 'package:app_payflow/modules/splash/splash_page.dart';
+import 'package:app_payflow/shared/themes/appcolors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: SplashPage(),
+    );
+  }
+}
