164dd13287593a233f19eceb15be443b313aec9b
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-04-22 14:09:51 -0300

    controle da página home

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,11 +1,23 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_print, non_constant_identifier_names
 
+import 'package:app_payflow/modules/home/home_controller.dart';
 import 'package:app_payflow/shared/themes/app_text_styles.dart';
 import 'package:app_payflow/shared/themes/appcolors.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +53,46 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.home,
+                color: AppColors.primary,
+              )),
+          GestureDetector(
+            onTap: () {
+              print("clicou");
+            },
+            child: Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5)),
+              child: Icon(
+                Icons.add_box_outlined,
+                color: AppColors.background,
+              ),
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              )),
+        ]),
       ),
     );
   }
