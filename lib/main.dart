981ae6d55df7ec7251a688d71c8ef32d5952ff30
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-04-22 09:08:17 -0300

    firebase no android

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
